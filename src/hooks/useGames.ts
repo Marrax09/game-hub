3ab926716e9153aa3ea490e9e4cfafc8c598dc55
Marcel Krone <Marcel.Krone@moysies.de>
2024-03-22 18:22:36 +0100

    Extract a query object.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,6 +3,7 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
+import { GameQuery } from "../App";
 
 export interface Platform {
   id: number;
@@ -19,11 +20,11 @@ export interface Game {
   }
 
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>('/games', { 
+const useGames = ( gameQuery: GameQuery) => useData<Game>('/games', { 
   params: { 
-    genres: selectedGenre?.id, 
-    parent_platforms: selectedPlatform?.id
+    genres: gameQuery.genre?.id, 
+    parent_platforms: gameQuery.platform?.id
   } }, 
-  [selectedGenre?.id, selectedPlatform?.id]);
+  [gameQuery]);
 
 export default useGames;
