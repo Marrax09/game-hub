20b0dd9947b7a14a81af3fafbca5d1e69d07836b
Marcel Krone <Marcel.Krone@moysies.de>
2024-04-02 10:58:47 +0200

    Color Mode switch to green.

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -6,7 +6,7 @@ const ColorModeSwitch = () => {
   return (
     <HStack>
       <Switch
-        colorScheme="yellow"
+        colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
