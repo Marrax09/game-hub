453051f17cc39331ec82a8eb8bfca41488ed7c38
Marcel Krone <Marcel.Krone@moysies.de>
2024-06-17 17:47:20 +0200

    Implement infinite Scrolling.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -30,7 +30,8 @@ const useGames = ( gameQuery: GameQuery) =>
             }),
           getNextPageParam: (lastPage, allePages) => {
             return lastPage.next ? allePages.length + 1 : undefined;
-          }
+          },
+          staleTime: 24 * 60 * 60 * 1000 // 24h
   });
 
 export default useGames;
